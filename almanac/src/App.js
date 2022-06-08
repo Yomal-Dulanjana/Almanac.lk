fce885a04a69616ff498db15d6a96ecba8e35bc1
Yomal-Dulanjana <65000757+Yomal-Dulanjana@users.noreply.github.com>
2022-06-08 20:26:51 +0530

    File Setup

--- almanac/src/App.js
+++ almanac/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./About/About";
+import "./App.css";
+import News from "./News/News";
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
+import Home from "./Home/Home";
+import Team from "./Team/Team";
+import Service from "./Service/Service";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Home />
+       <About />
+       <Service />
+       <Team />
+       <News />
+       <Contact />
+       <Footer />
     </div>
   );
 }
